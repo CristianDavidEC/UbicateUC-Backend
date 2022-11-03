ec067d1901ab458c0d3b4a16ad0dc67915e316cc
juliorc03-student <julianrivera003@gmail.com>
2022-11-03 15:54:50 -0500

    Algunas operaciones HTTP

--- src/models/SitiosModel.py
+++ src/models/SitiosModel.py
@@ -1,13 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from bson import ObjectId
 
 
 class Sitio(BaseModel):
     _id: ObjectId
-    nombre: str
-    descripcion: str
-    latitud: str
-    longitud: str
-    tipo: str
-    sede: str
-    estado: str
+    nombre: str = Field(...)
+    descripcion: str = Field(...)
+    latitud: str = Field(...)
+    longitud: str = Field(...)
+    tipo: str = Field(...)
+    sede: str = Field(...)
+    estado: str = Field(...)
+    bloque: str = Field(...)
+    piso: str = Field(...)
+
--- src/routes/SitiosRoutes.py
+++ src/routes/SitiosRoutes.py
@@ -1,15 +1,23 @@
 # ------ FastAPI -------------
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Body
 # ------ Libraries -----------
 from config.MongoDB import db
 from bson import ObjectId
 # ------ Dependencies -------
 from models.SitiosModel import Sitio
-from schemas.SitioSchema import sitio_entity, sitios_all
-
+from schemas.SitioSchema import *
 
 sitio = APIRouter()
 
+@sitio.post("/sitios",
+            tags = ["Sitios"],
+            status_code = status.HTTP_201_CREATED,
+            response_model=list[Sitio])
+async def crearSitio(sitio: Sitio = Body(...)):
+    nuevoSitio = dict(sitio)
+    db.Sitios.insert_one(nuevoSitio)
+    #status en caso de fallo
+    return sitio
 
 @sitio.get("/sitios",
            tags=["Sitios"],
@@ -29,3 +37,12 @@ async def get_all_sitios():
 async def get_sitio(id: str):
     sitio = db.Sitios.find_one({'_id': ObjectId(id)})
     return sitio_entity(sitio)
+
+@sitio.get("/sitios/{sede}/{bloque}/{piso}",
+           tags=["Sitios"],
+           status_code=status.HTTP_200_OK,
+           response_model=list[Sitio]
+        )
+async def get_sitio_for(sede: str, bloque: str, piso: str):
+    sitios = db.Sitios.find()
+    return filter_for(sitios_all(sitios), sede, bloque, piso)
